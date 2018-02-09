da336b7863d5f66a923de54002182b758100e183
michaelyangzz <michaelyangzz@msn.cn>
2018-02-09 13:22:44 +0800

    88

--- myserver/myserver/Startup.cs
+++ myserver/myserver/Startup.cs
@@ -1,50 +1,45 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
 
 namespace myserver
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.Use(async (context, next) =>
+            {
+                await Task.Delay(1500);
+                await next.Invoke();
+            });
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(builder => builder.WithOrigins("*").WithHeaders("Content-Type").WithMethods("GET", "PUT", "DELETE", "POST"));
+
+            app.UseMvc();
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddMvc();
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-
-      app.Use(async (context, next) =>
-      {
-        await Task.Delay(1500);
-        await next.Invoke();
-      });
-
-
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseCors(builder => builder.WithOrigins("*").WithHeaders("Content-Type").WithMethods("GET", "PUT", "DELETE", "POST"));
-
-      app.UseMvc();
-    }
-  }
 }
