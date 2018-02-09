a1e8fa452259a39a519124d17708a1f206a79343
michaelyangzz <michaelyangzz@msn.cn>
2018-02-09 15:44:00 +0800

    88

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Spinkit } from 'ng-http-loader/spinkits'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  spinkit = Spinkit;
+
+//skChasingDots: string;
+//skCubeGrid: string;
+//skDoubleBounce: string;
+//skRotatingPlane: string;
+//skSpinnerPulse: string;
+//skThreeBounce: string;
+//skWanderingCubes: string;
+//skWave: string;
+  //[backgroundColor] = "'#ff0000'"
+  //[spinner] = "spinkit.skCubeGrid"
+  //[debounceDelay] = "0" [filteredUrlPatterns]="['api']"
+
   title = 'app';
 }
